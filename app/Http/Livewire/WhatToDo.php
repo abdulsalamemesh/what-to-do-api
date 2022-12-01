bf4d57bee9d9b5b3ec95aa32750f0da60ee6691e
Abdulsalam Emesh <abdulsalam.emesh.work@gmail.com>
2022-12-01 10:33:37 +0100

    finish home page

--- app/Http/Livewire/WhatToDo.php
+++ app/Http/Livewire/WhatToDo.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Enums\CategoriesEnum;
 use App\Models\Task;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 
 class WhatToDo extends Component
@@ -13,22 +14,38 @@ class WhatToDo extends Component
     public array $persons;
     public array $costs = ['free', '$', '$$', '$$$'];
     public array $selectedCosts = ['free'];
+    public array $selectedPersons = [1];
+    public array $selectedCategories = ['staying busy'];
 
     public function mount()
     {
-        $this->task = Task::inRandomOrder()->first();
+        $this->task = Task::query()->where(function (Builder $eloquentBuilder) {
+            $eloquentBuilder->whereIn('cost', $this->selectedCosts)
+                ->whereIn('person', $this->selectedPersons)
+                ->whereIn('category', $this->selectedCategories);
+        })->inRandomOrder()->first();
         $this->categories = CategoriesEnum::values();
-        $this->persons = Task::query()->pluck('person')->unique()->toArray();
+        $this->persons = array_values(Task::query()->pluck('person')->unique()->toArray());
+        sort($this->persons);
     }
 
     public function render()
     {
-//        dd($this->categories);
         return view('livewire.what-to-do');
     }
 
     public function getTask()
     {
-        $this->task = Task::inRandomOrder()->first();
+        $this->task = Task::query()->where(function (Builder $eloquentBuilder) {
+            if ($this->selectedCosts) {
+                $eloquentBuilder->whereIn('cost', $this->selectedCosts);
+            }
+            if ($this->selectedPersons) {
+                $eloquentBuilder->whereIn('person', $this->selectedPersons);
+            }
+            if ($this->selectedCategories) {
+                $eloquentBuilder->whereIn('category', $this->selectedCategories);
+            }
+        })->inRandomOrder()->first();
     }
 }
