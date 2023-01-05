2471966e41c22ccc6e689d0479efc0f56417fc52
Abdulsalam Emesh <abdulsalam.emesh.work@gmail.com>
2023-01-05 23:57:02 +0100

    wip

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\CategoriesEnum;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
